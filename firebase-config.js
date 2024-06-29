f3dbc185cf1fdb00a20ebd0a3a562b5fdd8fc51e
Unhenryable <155365644+Unhenryable01@users.noreply.github.com>
2024-06-29 21:16:57 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC_y9o1Z0j4gKh_fD7NE3Qv8Fgn2DJn-Ls",
+  authDomain: "swoop-1da9f.firebaseapp.com",
+  projectId: "swoop-1da9f",
+  storageBucket: "swoop-1da9f.appspot.com",
+  messagingSenderId: "93407846936",
+  appId: "1:93407846936:web:43f6425df526db0120196d",
+  measurementId: "G-GBLBRX20BF"
+};
